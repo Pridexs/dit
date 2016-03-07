26327cc3c9b6a57e04444e9012943c52b9855cae
pridexs <alehstk@gmail.com>
2016-03-07 19:05:09 +0000

    Changes

--- security/rsa/src/numbers.py
+++ security/rsa/src/numbers.py
@@ -22,27 +22,82 @@ def isPrime(num):
                     v = (v ** 2) % num
     return True
 
-def isReallyPrime(num):
-    i = 2
-    m = int(math.sqrt(num) + 1)
-    while (i < m):
-        if num % i == 0:
-            return False
-        i += 1
-    return True
+def gcd(a, b):
+    r = 1
+    while (r != 0):
+        r = a % b
+        a = b
+        b = r
 
-x = random.getrandbits(127) + (1 << 127)
-if (x % 2 == 0):
-    x = x + 1
+    return a
 
-while (not isPrime(x)):
-    x = random.getrandbits(128)
-    if (x % 2 == 0):
-        x = x + 1
-'''
-if (isReallyPrime(x)):
-    print(x)
-else:
-    print('Not Really')
-'''
-print(x)
+# Extended Euclidean Algorithm
+def egcd(a, b):
+    x,y, u,v = 0,1, 1,0
+    while a != 0:
+        q, r = b//a, b%a
+        m, n = x-u*q, y-v*q
+        b,a, x,y, u,v = a,r, u,v, m,n
+    gcd = b
+    return y
+
+# Message
+message = "attack at dawn"
+
+# Encrypted Message
+eMess = [ ]
+
+
+# Generating p
+p = random.getrandbits(127) + (1 << 127)
+if (p % 2 == 0):
+    p += 1
+
+while (not isPrime(p)):
+    p = random.getrandbits(127) + (1 << 127)
+    if (p % 2 == 0):
+        p += 1
+
+# Generating q
+q = random.getrandbits(127) + (1 << 127)
+if (q % 2 == 0):
+    q += 1
+while (not isPrime(q)):
+    q = random.getrandbits(127) + (1 << 127)
+    if (q % 2 == 0):
+        q += 1
+
+# Generating n
+n = p * q
+
+# Generating e
+x = (p-1) * (q-1)
+e = 65537
+while True:
+    e = e + 1
+    if (gcd(x, e) == 1):
+        break
+
+# Generating d
+d = egcd(x, e)
+
+print('p = ' + str(p))
+print('q = ' + str(q))
+print('n = ' + str(n))
+print('x = ' + str(x))
+print('e = ' + str(e))
+print('d = ' + str(d))
+
+# Encryption
+for i in range(0, len(message)):
+    print(len(message), i)
+    eMess.append(( (ord(message[i]) ** e) % n))
+
+# Decryption
+print(eMess)
+
+for i in range(0, len(eMess)):
+    print(eMess[i])
+    #char = (eMess[i] ** d) % n
+    #print(char)
+    #print(str(unichr(char)))
