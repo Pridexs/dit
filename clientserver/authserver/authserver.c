36deff04526e01cf4205bd2073c60c6af335363e
Pridexs <alehstk@gmail.com>
2016-09-06 08:53:08 -0300

    Converting tabs to spaces

--- clientserver/authserver/authserver.c
+++ clientserver/authserver/authserver.c
@@ -9,8 +9,8 @@ int main(int argc, char **argv)
     int                 listenfd, connfd, n;
     struct sockaddr_in  servaddr;
     char                in_buff[MAXLINE];
-    char				out_buff[MAXLINE];
-    char				user[64], pass[64];
+    char                out_buff[MAXLINE];
+    char                user[64], pass[64];
     char                cliIp[MAXLINE];
     struct sockaddr_in  cliaddr;
     socklen_t           len;
@@ -36,28 +36,28 @@ int main(int argc, char **argv)
         inet_ntop(AF_INET, &cliaddr.sin_addr, cliIp, sizeof(cliIp));
 
         while( (n = read(connfd, in_buff, MAXLINE)) > 0)
-		{
-			if (strstr(in_buff, "\r\n\r\n"))
-			{
-				break;
-			}
-		}
-		if (n < 0)
-			err_sys("read error");
+        {
+            if (strstr(in_buff, "\r\n\r\n"))
+            {
+                break;
+            }
+        }
+        if (n < 0)
+            err_sys("read error");
 
-		sscanf(in_buff, "username: %s password: %s", user, pass);
+        sscanf(in_buff, "username: %s password: %s", user, pass);
         if (strcmp(user, "admin") == 0 && strcmp(pass, "pass") == 0)
-		{
-			snprintf(out_buff, sizeof(out_buff),
+        {
+            snprintf(out_buff, sizeof(out_buff),
             "PROCEED, Your IP Address is: %s and your Port Number is: %d\r\n",
             cliIp, ntohs(cliaddr.sin_port));
-		}
-		else
-		{
-			snprintf(out_buff, sizeof(out_buff), "DENIED\r\n");
-		}
+        }
+        else
+        {
+            snprintf(out_buff, sizeof(out_buff), "DENIED\r\n");
+        }
 
-		Write(connfd, out_buff, strlen(out_buff));
+        Write(connfd, out_buff, strlen(out_buff));
 
         Close(connfd);
     }
